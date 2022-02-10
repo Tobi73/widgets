9a1ca5933f72f2c394426013ee4417744739f6e0
Andrey Zaytsev <gman007@yandex.ru>
2022-02-10 20:36:11 +0400

    Added custom of exception for construction of invalid widgets

---
+++ src/main/java/com/azaitsev/widgets/entity/exceptions/InvalidChangeSetException.java
@@ -0,0 +1,8 @@
+package com.azaitsev.widgets.entity.exceptions;
+
+public class InvalidChangeSetException extends RuntimeException {
+
+    public InvalidChangeSetException(String message) {
+        super(message);
+    }
+}
